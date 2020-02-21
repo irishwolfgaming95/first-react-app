705780d4d6829178834f10a67fe70ba3a760c08e
irishwolfgaming95 <valkyire8@gmail.com>
2020-02-20 20:46:01 -0600

    first experiment

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import "./App.css";
 
+
+const names = ['Derek', 'Robby', 'Jake'];
+const welcomeAll = names.map((name) => <Welcome name={name} key={name}></Welcome>
+)
+
+
 function App() {
   return (
     <div className="App">
+       {welcomeAll}
       <header className="App-header">
         <p> Let the experimenting begin. </p>
       </header>
     </div>
   );
 }
+function Welcome(props) {
+  return (<h1>Hello, {props.name}</h1>
 
-export default App;
+  )
+};
+export default App ;
